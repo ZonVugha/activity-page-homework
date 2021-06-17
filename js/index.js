dccb22d1de3bdf88b8da82bee96a07bb599a5e04
Khaki46 <39086187+Khaki46@users.noreply.github.com>
2021-06-17 19:48:34 +0800

    Add

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ const span = document.querySelectorAll('span');
 for(let i = 0;i<span.length;i++){
     span[i].index=i;
     span[i].onclick=function go(e){
-        thumbSon.style.left=-590 * this.index +'px';
+        thumbSon.style.left=-35 * this.index +'vw';
         for(let item of span){
             item.className="";
         }
@@ -30,7 +30,7 @@ setInterval(() => {
 if(num>=3){
     num = 0;
 }
-thumbSon.style.left=-590 * span[num].index +'px';
+thumbSon.style.left=-35 * span[num].index +'vw';
 for(let item of span){
     item.className="";
 }
@@ -48,4 +48,30 @@ for (let index = 0; index < a.length; index++) {
       console.log(status);
       sessionStorage.setItem('status', status);
     }
+}
+
+const getAllURL = "http://127.0.0.1:8086/activity/activity/getAll";
+const requestAll = new XMLHttpRequest();
+const showBox = document.getElementById('showBox');
+requestAll.open('GET', getAllURL);
+requestAll.responseType = 'json';
+requestAll.send();
+requestAll.onload = function () {
+    let getAllData = requestAll.response;
+    
+    for (let index = 0; index < getAllData.length; index++){
+        const newDiv = document.createElement('div');
+        const newDiv2 = document.createElement('div');
+        const newImage = document.createElement('img');
+        const h4 = document.createElement('h4');
+        newDiv.className = "content";
+        newDiv2.className = "mask";
+        newImage.setAttribute('src', getAllData[index].activityPosters);
+        h4.innerHTML = getAllData[index].activityTitle;
+        showBox.appendChild(newDiv);
+        newDiv.appendChild(newImage);
+        newDiv.appendChild(newDiv2);
+        newDiv2.appendChild(h4);
+    }
+
 }
